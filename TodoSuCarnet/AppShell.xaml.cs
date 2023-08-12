c7d52d319712d995148db1fab62c713c2b2d5e7f
EmersonAguilar <eneftaly1911@gmail.com>
2023-08-12 13:26:10 -0600

    Agregar archivos de proyecto.

---
+++ TodoSuCarnet/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using TodoSuCarnet.Views;
+
+namespace TodoSuCarnet;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+        Routing.RegisterRoute(nameof(TodoItemPage), typeof(TodoItemPage));
+    }
+}
